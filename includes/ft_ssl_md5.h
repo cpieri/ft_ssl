d0cc8863c363450cd3b5d92c86c19c48ee915313
Clement PIERI <cpieri@e3r1p8.42.fr>
2019-01-26 19:16:04 +0100

    test padding

--- includes/ft_ssl_md5.h
+++ includes/ft_ssl_md5.h
@@ -6,7 +6,7 @@
 /*   By: cpieri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 17:04:27 by cpieri            #+#    #+#             */
-/*   Updated: 2019/01/25 17:54:26 by cpieri           ###   ########.fr       */
+/*   Updated: 2019/01/26 18:30:54 by cpieri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,25 @@
 # define FT_SSL_MD5_H
 
 # include "../libft/include/libft.h"
+
+# define	FAILURE			-1
+# define	SUCCESS			0
+# define	OP_TAB_LEN		64
+# define	BLOCK_TAB_LEN	16
+
+typedef struct		s_block
+{
+	int				tab[BLOCK_TAB_LEN];
+}					t_block;
+
+typedef	struct		s_md5
+{
+	char*			org_str;
+	size_t			org_len;
+	size_t			org_nb_bits;
+	int				nb_blocks;
+}					t_md5;
+
 
 int		parsing(int ac, char **av);
 void	print_usage(const int usage_int, const char *command);
--- srcs/md5.c
+++ srcs/md5.c
@@ -6,43 +6,52 @@
 /*   By: cpieri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/25 15:18:34 by cpieri            #+#    #+#             */
-/*   Updated: 2019/01/25 18:35:09 by cpieri           ###   ########.fr       */
+/*   Updated: 2019/01/26 18:44:47 by cpieri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ssl_md5.h"
 #include <stdio.h>
 
-static int*		stoi(char *s)
+static int		padding(t_md5 md5)
 {
-	int		*ret;
-	int		i;
+	t_block		*blocks;
+	int			i;
+	int			y;
 
 	i = 0;
-	if (!(ret = (int*)malloc(sizeof(int) * (ft_strlen(s) + 1))))
-		return (NULL);
-	while (s[i])
-		ret[i] = (int)s[i];
-	return (ret);
+	if (!(blocks = (t_block*)malloc(sizeof(t_block) * (md5.nb_blocks + 1))))
+		return (FAILURE);
+	while (i <= md5.nb_blocks)
+	{
+		y = 0;
+		while (y < BLOCK_TAB_LEN)
+		{
+			blocks->tab[y++] = 0;
+		}
+		i++;
+	}
+	int new_len;
+	for(new_len = md5.org_len *8 + 1; new_len%512!=448; new_len++);
+	printf("new_len = %d\n", new_len);
+	new_len /= 8;
+	printf("new_len = %d\n", new_len);
+	return (SUCCESS);
 }
 
-static void		padding(char *s, int len)
+void			md5(char *s)
 {
-	int		*msg;
-	int		nb_bits;
-	int		do_padding;
+	t_md5		md5;
+	const int	op_tabs[OP_TAB_LEN] = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17,
+		22, 7, 12, 17, 22, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14,
+		20, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 6, 10,
+		15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
 
-	msg = stoi(s);
-	nb_bits = len * 8;
-	msg = msg << 1 | 1;
-	do_padding = 512 - 64 - 1 - nb_bits;
-	printf("%s have %u bits, we do padding %u\n", s, nb_bits, do_padding);	
-}
-
-void			md5(char* s)
-{
-	int		len;
-
-	len = ft_strlen(s);
-	padding(s, len);
+	(void)op_tabs;
+	md5.org_len = ft_strlen(s);
+	md5.org_nb_bits = md5.org_len * 8;
+	md5.nb_blocks = md5.org_nb_bits / 512;
+	md5.org_str = ft_strdup(s);
+	printf("len: %zu\n", md5.org_len);
+	padding(md5);
 }
