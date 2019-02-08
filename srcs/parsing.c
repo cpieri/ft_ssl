0ad05aa1da6ac547f1673268f9008c7d3cc19e4f
Clement PIERI <cpieri@e2r2p7.42.fr>
2019-02-08 15:53:03 +0100

    lol

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: cpieri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 17:29:57 by cpieri            #+#    #+#             */
-/*   Updated: 2019/02/08 13:26:38 by cpieri           ###   ########.fr       */
+/*   Updated: 2019/02/08 14:24:08 by cpieri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,9 +50,11 @@ int		parsing(int ac, char **av)
 	t_parse		*parsing;
 
 	if (!(parsing = (t_parse*)ft_memalloc(sizeof(t_parse) * 1)))
-		exit_error("Error: Func Parsing: Memalloc failed");
+		exit_error("Error: Func parsing: Memalloc failed");
 	else if ((parsing->hash_type = check_command(av[1])) == FAILURE)
 		print_usage(INVALID_CMD, av[1]);
+	else if (!(parsing->lst_opts = (t_opt**)ft_memalloc(sizeof(t_opt*) * 1)))
+		exit_error("Error: Func parsing: Memalloc failed");
 	parse_opts(ac, av, parsing);
 	return (SUCCESS);
 }
