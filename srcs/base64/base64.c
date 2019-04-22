7b4174a3f08fd048bd690bc87ca541ac0203d0ca
Louise P <cpieri@student.42.fr>
2019-04-22 10:07:09 +0200

    Base64 in progress:
    Base64 decode in progress

--- srcs/base64/base64.c
+++ srcs/base64/base64.c
@@ -6,7 +6,7 @@
 /*   By: cpieri <cpieri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/12 16:53:29 by cpieri            #+#    #+#             */
-/*   Updated: 2019/04/15 15:17:39 by cpieri           ###   ########.fr       */
+/*   Updated: 2019/04/22 10:03:25 by cpieri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,10 @@ void			*base64(void *opts, size_t len_opts)
 
 	(void)len_opts;
 	opt = (t_opt*)opts;
-	if ((opt->flags.b64_flags & e_base64_encode))
+	print_lst(&opt);
+	if ((opt->flags.b64_flags & e_base64_encode) == e_base64_encode)
 		base64_encode((uint8_t*)opt->data->data, opt->data->len_data);
-	else if ((opt->flags.b64_flags & e_base64_decode))
+	else if ((opt->flags.b64_flags & e_base64_decode) == e_base64_decode)
 		base64_decode((uint8_t*)opt->data->data, opt->data->len_data);
 	return (opts);
 }
--- srcs/base64/base64_decode.c
+++ srcs/base64/base64_decode.c
@@ -6,26 +6,32 @@
 /*   By: cpieri <cpieri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 13:41:31 by cpieri            #+#    #+#             */
-/*   Updated: 2019/04/15 15:16:01 by cpieri           ###   ########.fr       */
+/*   Updated: 2019/04/22 09:53:34 by cpieri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "base64/base64.h"
 #include <stdio.h>
 
-uint8_t		*base64_decode(uint8_t *msg, size_t len)
+static size_t	b64_len_decode(uint8_t *s)
 {
-	size_t			i;
-	t_base64_encode	n;
+	size_t	new_len;
+	uint8_t	*tmp;
 
-	i = 0;
+	tmp = s;
+	while (g_base64_d[*(tmp++)] <= 63);
+	new_len = (tmp - s) - 1;
+	printf("new_len = %zu\n", new_len);
+	return (((new_len + 3) / 4) * 3);
+}
+
+uint8_t			*base64_decode(uint8_t *msg, size_t len)
+{
+	size_t	len_plain;
+
+	(void)len;
 	printf("decode func\n");
-	y = 0;
-	while (i < len)
-	{
-		if (ft_isspace(msg[i]))
-			i++;
-		i++;
-	}
+	len_plain = b64_len_decode(msg);
+	printf("len_plain = %zu\n", len_plain);
 	return (msg);
 }
