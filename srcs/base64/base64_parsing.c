531317e9156be480d9cc70110f02a8772c4679a0
Louise P <cpieri@student.42.fr>
2019-05-03 17:07:57 +0200

    b64 refactor parsing

--- srcs/base64/base64_parsing.c
+++ srcs/base64/base64_parsing.c
@@ -6,21 +6,19 @@
 /*   By: cpieri <cpieri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 16:56:03 by cpieri            #+#    #+#             */
-/*   Updated: 2019/05/03 13:47:46 by cpieri           ###   ########.fr       */
+/*   Updated: 2019/05/03 15:01:37 by cpieri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ssl.h"
 #include "base64/base64.h"
 
-t_data	*get_b64_opt(char **av, int *now, t_opt *t_o, char **output)
+static void		get_b64_opt(char **av, int *now, t_opt *t_o, char **output)
 {
-	t_data		*data;
 	size_t		len_now;
 	size_t		i;
 
 	i = 0;
-	data = NULL;
 	len_now = ft_strlen(av[*now]);
 	while (++i < len_now)
 	{
@@ -28,7 +26,7 @@ t_data	*get_b64_opt(char **av, int *now, t_opt *t_o, char **output)
 			get_b64_ed(&(t_o->data), &(t_o->flags),
 			(av[*now + 1] != NULL ? av[*now + 1] : NULL), av[*now][i]);
 		else if (av[*now][i] == 'i')
-			get_b64_i(&data, &(t_o->flags), av[(*now) + 1], now);
+			get_b64_i(&(t_o->data), &(t_o->flags), av[(*now) + 1], now);
 		else if (av[*now][i] == 'o')
 		{
 			t_o->flags.b64_flags |= e_base64_outputf;
@@ -36,28 +34,19 @@ t_data	*get_b64_opt(char **av, int *now, t_opt *t_o, char **output)
 			(*now)++;
 		}
 	}
-	return (data);
 }
 
-t_opt	*get_base64_args(const int ac, char **av, int now)
+static t_opt	*get_base64_args(const int ac, char **av, int now)
 {
 	t_opt	t_o;
-	t_data	*tmp;
 	char	*fd_output;
 
 	fd_output = NULL;
-	tmp = NULL;
+	t_o = (t_opt){NULL, {0, 0, 0, 0, 0}, NULL};
 	while (now < ac)
 	{
 		if (av[now][0] == '-')
-		{
-			tmp = get_b64_opt(av, &now, &t_o, &fd_output);
-			if (tmp != NULL)
-			{
-				clean_data(&(t_o.data));
-				t_o.data = tmp;
-			}
-		}
+			get_b64_opt(av, &now, &t_o, &fd_output);
 		else
 			get_b64_ed(&(t_o.data), &(t_o.flags), av[now], 'e');
 		now++;
@@ -67,7 +56,7 @@ t_opt	*get_base64_args(const int ac, char **av, int now)
 	return (new_opt_fd(t_o.flags, t_o.data, fd_output));
 }
 
-t_opt	*base64_opts(const int ac, char **av, t_opt *opts, int now)
+t_opt			*base64_opts(const int ac, char **av, t_opt *opts, int now)
 {
 	t_opt	*new;
 
