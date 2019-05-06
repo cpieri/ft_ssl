c8d3ed56ca107a90bb7a6c04bf46ec348e42dc3e
Louise P <cpieri@student.42.fr>
2019-05-06 16:24:26 +0200

    check flags

--- srcs/symmetric/symmetric_parsing.c
+++ srcs/symmetric/symmetric_parsing.c
@@ -6,7 +6,7 @@
 /*   By: cpieri <cpieri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 12:04:37 by cpieri            #+#    #+#             */
-/*   Updated: 2019/05/06 15:26:59 by cpieri           ###   ########.fr       */
+/*   Updated: 2019/05/06 16:18:11 by cpieri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,33 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/*
+else if (av[*now][i] == 'o')
+{
+	t_o->flags.b64_flags |= e_base64_outputf;
+	*output = (av[(*now) + 1] != NULL) ? av[(*now) + 1] : NULL;
+	(*now)++;
+}
+*/
+
 static void		get_sym_opt(char **av, int *now, t_opt *opt)
 {
-	(void)av;
-	(void)now;
+	size_t		len_now;
+	size_t		i;
+
+	i = 0;
 	(void)opt;
+	len_now = ft_strlen(av[*now]);
+	while (++i < len_now)
+	{
+		while (g_sym_opt[y].opt != 0)
+		{
+			if (g_sym_opt[y].opt == av[*now][i])
+				printf("opt: %c", av[*now][i]);
+				// g_sym_opt[y].f();
+			y++;
+		}
+	}
 }
 
 static t_opt	*get_sym_args(const int ac, char **av, int now)
